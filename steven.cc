2a1a1689f4f7bed074c7dfba0319ba67835b8416
OskarBollner <oskar.bollner@gmail.com>
2023-10-03 19:09:18 +0200

    Added testfiles for Person and Bank

--- steven.cc
+++ steven.cc
@@ -15,21 +15,30 @@ int Bank::get_expences()
 {
     return tot_expences;
 }
+
+void Person::get_name();
+{
+    return name;
+}
+
 void Person::update_debt(int cost)
 //  ----------------------
 {
     debt -= cost;
 }
+
 int Person::get_debt() const
 //  ----------------------
 {
     return debt;
 }
+
 int Person::get_expence() const
 //  ----------------------
 {
     return expence;
 }
+
 void Person::update_expence(int cost)
 //  ----------------------
 {
---
+++ test_Person.cc
@@ -0,0 +1,18 @@
+#include "steven.h"
+#include "steven.h"
+#include "catch.hpp"
+
+TEST_CASE("Person class")
+{
+    SECTION("empty")
+    {
+        Person p1 {};
+        CHECK(p1.get_expence() == 0);
+        CHECK(p1.get_debt() == 0);
+        
+        p1.update_debt(50);
+        CHECK(p1.get_debt() == 50);
+        p1.update_expence(50);
+        CHECK(p1.get_expence() == 50);
+    }
+}
